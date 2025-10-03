61692f075db5feb53e190e9767698ee119d460e4
Navi2004725 <naveeshabeydeera123@gmail.com>
2025-10-04 00:43:41 +0530

    commit 3 ✏️

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import stylr from "./contactus.module.css";
+import Link from "next/link";
 
 function page() {
-  return <h1 className={stylr.link}>Contact us</h1>;
+  return (
+    <>
+      <h1 className={stylr.link}>Contact us</h1>
+      <Link href="/">Back to home</Link>
+    </>
+  );
 }
 
 export default page;
